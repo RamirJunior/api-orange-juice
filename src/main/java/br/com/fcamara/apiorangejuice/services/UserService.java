9fcde763fe08b6b9eb5fcb1ccfec106bc0ccc1ad
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-01-24 16:03:42 -0300

    transactional annotation to the UserService class

--- src/main/java/br/com/fcamara/apiorangejuice/services/UserService.java
+++ src/main/java/br/com/fcamara/apiorangejuice/services/UserService.java
@@ -8,38 +8,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
-    // Find all function
-    @Transactional(readOnly = true)
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    // Find by ID function
-    @Transactional(readOnly = true)
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
 
-    // Save User
-    @Transactional
+
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
-    // Delete by ID
-    @Transactional
     public void deleteById(Long id) {
         userRepository.deleteById(id);
     }
-
-
-
 
 }
