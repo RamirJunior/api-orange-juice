d8da172d05290c6e483f7133dfdaf26524a2281c
Ramir Junior <junior.ribeiro0@live.com>
2024-01-27 01:32:42 -0300

    feat: Adding postgres and docker configuration

--- src/main/java/br/com/fcamara/apiorangejuice/domain/entity/User.java
+++ src/main/java/br/com/fcamara/apiorangejuice/domain/entity/User.java
@@ -1,19 +1,17 @@
 package br.com.fcamara.apiorangejuice.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
-@Entity(name = "users")
+@Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String firstname;
     private String lastname;
     private String email;
     private String password;
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into users (id, email, lastname, name, password) values (600,'gandalf@cinzento.com','O Branco','Gandalf','1234');
-insert into users (id, email, lastname, name, password) values (601,'frodo@bolseiro.com','Bolseiro','Frodo','1234');
-
