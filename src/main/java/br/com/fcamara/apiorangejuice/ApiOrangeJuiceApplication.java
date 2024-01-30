6722c81276e4a4a569ae34e9fe080995a5c5212d
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-01-29 21:58:57 -0300

    doc: added Swagger documentation

--- src/main/java/br/com/fcamara/apiorangejuice/ApiOrangeJuiceApplication.java
+++ src/main/java/br/com/fcamara/apiorangejuice/ApiOrangeJuiceApplication.java
@@ -1,5 +1,7 @@
 package br.com.fcamara.apiorangejuice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
