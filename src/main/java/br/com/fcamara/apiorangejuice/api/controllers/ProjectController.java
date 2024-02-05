667b1b456e02a260c36dfe51b699b2077957809c
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-02-05 14:55:10 -0300

    Revert "doc: translated and added swagger documentation"
    This reverts commit d152d5901c98c274f8f6d5c3532f611b36617e54.

--- src/main/java/br/com/fcamara/apiorangejuice/api/controllers/ProjectController.java
+++ src/main/java/br/com/fcamara/apiorangejuice/api/controllers/ProjectController.java
@@ -3,14 +3,10 @@ package br.com.fcamara.apiorangejuice.api.controllers;
 import br.com.fcamara.apiorangejuice.api.dtos.project.ProjectRequest;
 import br.com.fcamara.apiorangejuice.api.dtos.project.ProjectResponse;
 import br.com.fcamara.apiorangejuice.services.ProjectService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,45 +16,28 @@ import static br.com.fcamara.apiorangejuice.api.utils.Constants.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(PROJECT_CONTROLLER_MAPPING_ROUTE)
-@Tag(name = "ProjectController", description = "Endpoints relacionados aos projetos")
 public class ProjectController {
 
     private final ProjectService projectService;
 
-    @Operation(summary = "Salvar um novo projeto para um usuário", description = "Recurso para salvar um novo projeto para um usuário. Requisição exige um Bearer Token. Acesso restrito para qualquer papel.",
-            security = @SecurityRequirement(name = "security")
-    )
-    @PreAuthorize("hasRole('USER')")
     @PostMapping
     public ResponseEntity<ProjectResponse> saveProject(@Valid @RequestBody ProjectRequest projectRequest) {
         var savedProject = projectService.saveProject(projectRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProject);
     }
 
-    @Operation(summary = "Buscar todos os projetos", description = "Recurso para buscar todos os projetos cadastrados. Requisição exige um Bearer Token. Acesso restrito para qualquer papel.",
-            security = @SecurityRequirement(name = "security")
-    )
-    @PreAuthorize("hasRole('USER')")
     @GetMapping(GET_ALL_PROJECTS_ROUTE)
     public ResponseEntity<List<ProjectResponse>> findAllProjects() {
         var projectList = projectService.findAllProjects();
         return ResponseEntity.status(HttpStatus.OK).body(projectList);
     }
 
-    @Operation(summary = "Buscar todos os projetos do usuário por id", description = "Recurso para fazer a busca de todos os projetos cadastrados do usuário. Requisição exige um Bearer Token. Acesso restrito para USER",
-            security = @SecurityRequirement(name = "security")
-    )
-    @PreAuthorize("hasRole('USER')")
     @GetMapping
     public ResponseEntity<List<ProjectResponse>> findUserProjects() {
         var userProjects = projectService.findUserProjects();
         return ResponseEntity.status(HttpStatus.OK).body(userProjects);
     }
 
-    @Operation(summary = "Atualizar um projeto existente", description = "Recurso para atualizar um projeto existente de um usuário. Requisição exige um Bearer Token. Acesso restrito para qualquer papel.",
-            security = @SecurityRequirement(name = "security")
-    )
-    @PreAuthorize("hasRole('USER')")
     @PutMapping
     public ResponseEntity<ProjectResponse> updateProject(@RequestBody ProjectRequest projectRequest) {
         var updatedProject = projectService.updateProject(projectRequest);
@@ -67,10 +46,6 @@ public class ProjectController {
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.CONFLICT).build());
     }
 
-    @Operation(summary = "Excluir um projeto", description = "Recurso para excluir um projeto de um usuário. Requisição exige um Bearer Token. Acesso restrito para qualquer papel.",
-            security = @SecurityRequirement(name = "security")
-    )
-    @PreAuthorize("hasRole('USER')")
     @DeleteMapping(DELETE_PROJECT_ROUTE)
     public ResponseEntity deleteProject(@PathVariable Long projectId) {
         var response = projectService.deleteProject(projectId);
--- src/main/java/br/com/fcamara/apiorangejuice/configs/SecurityConfig.java
+++ src/main/java/br/com/fcamara/apiorangejuice/configs/SecurityConfig.java
@@ -28,12 +28,6 @@ public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
 
-    private static final String[] DOCUMENTATION_OPENAPI = {
-            "/v3/api-docs/**",
-            "/swagger-ui-custom.html", "/swagger-ui.html", "/swagger-ui/**",
-            "/**.html", "/webjars/**", "/configuration/**", "/swagger-resources/**"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -42,7 +36,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/user/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
-                        .requestMatchers(DOCUMENTATION_OPENAPI).permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .cors(Customizer.withDefaults())
