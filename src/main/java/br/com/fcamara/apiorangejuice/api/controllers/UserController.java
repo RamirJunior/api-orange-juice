8d926e803d56dd4a36349b04f7d666aa3488db84
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-02-05 14:49:24 -0300

    Revert "doc: translated and added swagger documentation"
    This reverts commit d152d5901c98c274f8f6d5c3532f611b36617e54.

--- src/main/java/br/com/fcamara/apiorangejuice/api/controllers/UserController.java
+++ src/main/java/br/com/fcamara/apiorangejuice/api/controllers/UserController.java
@@ -7,10 +7,6 @@ import br.com.fcamara.apiorangejuice.api.dtos.user.UserResponse;
 import br.com.fcamara.apiorangejuice.domain.entities.User;
 import br.com.fcamara.apiorangejuice.services.TokenService;
 import br.com.fcamara.apiorangejuice.services.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,18 +22,12 @@ import static br.com.fcamara.apiorangejuice.api.utils.Constants.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(USER_CONTROLLER_MAPPING_ROUTE)
-@Tag(name = "UserController", description = "Endpoints relacionados aos usuários")
 public class UserController {
 
     private final UserService userService;
     private final TokenService tokenService;
     private final AuthenticationManager authenticationManager;
 
-    @Operation(summary = "Registrar um novo usuário", description = "Recurso para registrar um novo usuário na aplicação.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso"),
-            @ApiResponse(responseCode = "409", description = "Conflito - o usuário já existe")
-    })
     @PostMapping(POST_USER_REGISTER_ROUTE)
     public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRequest userRequest) {
         UserResponse savedUser = userService.register(userRequest);
@@ -45,11 +35,6 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
-    @Operation(summary = "Autenticar um usuário", description = "Recurso para autenticar um usuário na aplicação e obter um token de acesso.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Usuário autenticado com sucesso e token gerado"),
-            @ApiResponse(responseCode = "401", description = "Não autorizado - credenciais inválidas")
-    })
     @PostMapping(POST_USER_LOGIN_ROUTE)
     public ResponseEntity<SuccessLoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         var usernamePassword = tokenService.getAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
